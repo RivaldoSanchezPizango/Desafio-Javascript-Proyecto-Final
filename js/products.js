c4499d9bbe363df30555d615e3b28078c1fae826
Rivaldo Sanchez Pizango <rivaldosanchezp@gmail.com>
2022-11-21 18:48:33 -0300

    supermercado

---
+++ js/products.js
@@ -0,0 +1,66 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Harina",
+    precio: 260,
+    img:
+      "https://carrefourar.vtexassets.com/arquivos/ids/205215/7792180139313_02.jpg?v=637601556417500000",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Galletitas",
+    precio: 190,
+    img:
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/279/512/products/terrabusi-surtidas11-ce9195f484e533ef3816369342676300-480-0.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Cerveza",
+    precio: 450,
+    img:
+      "https://static.cotodigital3.com.ar/sitios/fotos/full/00047100/00047101.jpg?3.0.144",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Leche",
+    precio: 250,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZlJmBJvQ-EiJeukDVUgFlqwoo82_eJs7-7zqCzxL788Vd3bcj72yys9T3djBNwF8JS7Q&usqp=CAU",
+    cantidad: 1,
+  },
+  {
+    id: 5,
+    nombre: "Fideos",
+    precio: 220,
+    img:
+      "https://atomoconviene.com/atomo-ecommerce/951-home_default/fideos-largos-lucchetti-tallarin-500-grs-.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 6,
+    nombre: "Aceite",
+    precio: 670,
+    img:
+      "https://jumboargentina.vtexassets.com/arquivos/ids/591712/Aceite-Girasol-Pureza-0-9l-1-850473.jpg?v=637290848777400000",
+    cantidad: 1,
+  },
+  {
+    id: 7,
+    nombre: "Detergente",
+    precio: 170,
+    img:
+      "https://farmacityar.vteximg.com.br/arquivos/ids/221723-1000-1000/223850_detergente-magistral-multiuso-limon-x-750-ml_imagen-1.jpg?v=637866637943770000",
+    cantidad: 1,
+  },
+  {
+    id: 8,
+    nombre: "Arroz",
+    precio: 210,
+    img:
+      "https://cdn11.bigcommerce.com/s-abmjjefojj/images/stencil/1280x1280/products/2721/12352/Gallo-Arroz-Oro-500-White-Rice-Argentina_Select-1000x1000__45467.1600543052.jpg?c=1",
+    cantidad: 1,
+  },
+];
